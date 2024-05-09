67a1eedea62e79765073dd855cd211b222ecd9e8
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-05-08 22:45:53 -0300

    Add files via upload

--- Exercicios_material/src/Exercicio10.java
+++ Exercicios_material/src/Exercicio10.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
 public class Exercicio10 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		double valor, cont = 1, resultado = 0;
+		String resposta;
+
+		while (true) {
+			System.out.println("valor: ");
+			valor = in.nextInt();
+
+			while (cont != 0) {
+				resultado = valor * cont;
+				System.out.println(valor + " * " + cont + " = " + resultado);
+				cont++;
+			}
+			System.out.println("Deseja outra tabuada (s/n)? ");
+			resposta = in.next();
+			if (resposta.equalsIgnoreCase("n")) {
+				break;
+			}
+			cont = 1 ;
+		}
+
+	}
 
 }
--- Exercicios_material/src/Exercicio11.java
+++ Exercicios_material/src/Exercicio11.java
@@ -4,10 +4,12 @@ public class Exercicio11 {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		int valor, cont = 1, fat = 1;
+		int valor, cont = 1;
+		double fat = 1;
 		
 		System.out.println("Digite um valor inteiro e positivo");
 		valor = in.nextInt();
+		in.close();
 		
 		if(valor < 0) {
 			System.out.println("O valor deve ser inteiro e positivo");
