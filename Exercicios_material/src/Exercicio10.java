b4b8cdb63a8fef8bf52b4fc671e7c9d14999f43a
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-05-14 22:48:44 -0300

    Add files via upload

--- Exercicios_material/src/Exercicio10.java
+++ Exercicios_material/src/Exercicio10.java
@@ -4,14 +4,14 @@ public class Exercicio10 {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		double valor, cont = 1, resultado = 0;
+		double valor, cont = 0, resultado = 0;
 		String resposta;
 
 		while (true) {
 			System.out.println("valor: ");
 			valor = in.nextInt();
 
-			while (cont != 0) {
+			while (cont <= 10) {
 				resultado = valor * cont;
 				System.out.println(valor + " * " + cont + " = " + resultado);
 				cont++;
@@ -21,7 +21,7 @@ public class Exercicio10 {
 			if (resposta.equalsIgnoreCase("n")) {
 				break;
 			}
-			cont = 1 ;
+			cont = 0 ;
 		}
 
 	}
---
+++ Exercicios_material/src/Exercicio12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio12 {
+	public static void main(String[] args) {
+
+		
+		Scanner in = new Scanner(System.in);
+		int valor;
+		
+		System.out.println("valor inteiro e positivo: ");
+		valor = in.nextInt();
+		
+		for(int cont = -valor; cont <= valor; cont++) {
+			if (valor % cont == 0 ) {
+				System.out.println(cont + " ");
+			}
+		}
+				
+	}
+}
